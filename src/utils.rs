e49fbd2c44956431d5278ee3292115a68f9e70f7
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2024-01-21 01:17:14 +0100

    Add utils::string_to_square funtion

--- src/utils.rs
+++ src/utils.rs
@@ -5,6 +5,20 @@ use crate::{
 
 const FILE_LETTERS: [char; 8] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
+pub fn string_to_square(string: &str) -> Result<u8> {
+    let mut chars = string.chars();
+
+    let file = chars.next().ok_or(SquareParsingError::NotEnoughParts)?;
+    let rank = chars.next().ok_or(SquareParsingError::NotEnoughParts)?;
+
+    let file = get_file_number(file)? as u8;
+    let rank = rank.to_digit(10).ok_or(SquareParsingError::BadRank(rank))? as u8 - 1;
+
+    let square = rank * 8 + file;
+
+    Ok(square)
+}
+
 pub fn get_file_letter(file: u16) -> Result<char> {
     FILE_LETTERS
         .get(file as usize)
